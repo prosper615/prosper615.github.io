522c848f79c2df61151f97cfe7666408ac3d2eb6
prosper615 <prosperchiderachukwu@gmail.com>
2025-09-26 03:07:17 -0700

    Log3

--- Index.js
+++ Index.js
@@ -3,7 +3,8 @@
 
 const posts = [
   { title: "Learning as i go ( notes from my 1 month of research)", file: "Log1.html" },
-  { title: "The spark", file: "Log2.html" }
+  { title: "The spark", file: "Log2.html" },
+  { title: "Bounded : Pedersen as a commitment device", file: "Log3.html" },
 ];
 
 const postList = document.getElementById("post-list");
